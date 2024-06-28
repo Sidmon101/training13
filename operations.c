7bc9f0d06ca3c97641e9f991b4007b3cbfa671bb
sidmon101 <user08@trainux01.jrf11vejvqfuhbwnm0ewggipoa.ax.internal.cloudapp.net>
2024-06-28 06:06:50 +0000

    done some

--- operations.c
+++ operations.c
@@ -8,11 +8,15 @@ int main(){
 	int sq=num*num;
 	int dig=0;
 	int tem=num;
+	int s,sum=0;
 	printf("The Square of the Number:%d\n",sq);
 	while(tem!=0){
+		s=tem%10;
 		tem=tem/10;
 		dig++;
+		sum+=s;
 	}
+	printf("Sum of Digits: %d\n",sum);
 	printf("Number of Digits:%d\n",dig);
         int check=1;
         for(int i=0;i<dig;i++){
